b23b45e855b1eb2c4ac0f99378650236ff58364f
catupperman <catupperman@gmail.com>
2021-10-01 11:23:33 -0400

    views connections

---
+++ views/api.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+})
+
+
+
+module.exports = router;
