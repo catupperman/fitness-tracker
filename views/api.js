ddae040cf92e9dff60ffc817cd164a7c0fb7c059
catupperman <catupperman@gmail.com>
2021-10-06 17:56:10 -0400

    api routes-heroku

--- views/api.js
+++ views/api.js
@@ -2,9 +2,18 @@ const router = require("express").Router();
 const Workout = require("../models/Workout");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create({ })
+    Workout.create({}).then(dataDb => res.json(dataDb)).catch(err => res.json(err))
+})
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}},{new: true, runValidators: true}).then(dataDb => res.json(dataDb)).catch(err => res.json(err))
+} )
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([{$addFields: {totalDuration: {$sum: '$exercises.duration'}}}]).then(dataDb => res.json(dataDb)).catch(err => res.json(err))
 })
 
-
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([{$addFields: {totalDuration: {$sum: '$exercises.duration'}}}]).sort({_id: -1}).limit(7).then(dataDb => res.json(dataDb)).catch(err => res.json(err))
+})
 
 module.exports = router;
