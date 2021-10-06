cb776875ebc060971e0d307696a9790445a59776
catupperman <catupperman@gmail.com>
2021-10-06 13:17:52 -0400

    set up

--- views/api.js
+++ views/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/Workout");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create({})
+    Workout.create({ })
 })
 
 
